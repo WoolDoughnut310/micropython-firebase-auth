6bd21f887dabf87d38a1fe826c065fcec8b1ea41
WoolDoughnut310 <wooldoughnutspi@outlook.com>
2022-02-05 22:30:46 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="micropython-firebase-auth",
+    version="1.0.0",
+    author="WoolDoughnut310",
+    author_email="wooldoughnutspi@outlook.com",
+    description="Firebase Auth implementation for Micropython optimized for ESP32.",
+    url="https://github.com/WoolDoughnut310/micropython-firebase-auth",
+    packages=["firebase_auth"]
+)
